349578f841f37fcde5ad33e752b1031aa520d47f
Laura Gonzalez <l.gonzalezfernan@gmail.com>
2024-02-19 22:26:53 +0100

    Add proper button handling with OneButton

--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,88 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
+#include <OneButton.h>
 
 #include "ota.h"
 
-//Set the name of the bluetooth keyboard (that shows up in the bluetooth menu of your device)
+
 BleKeyboard bleKeyboard("RallyController");
 
-const int volDownButton = 0; // Trip down
-const int volUpButton = 1; // Trip up
-const int trackNextButton = 2; // RB down
-const int trackPrevButton = 3; // RB up
-const int extraButton = 4;
+OneButton switchDownButton(0, true, true); // Advance roadbook
+OneButton switchUpButton(1, true, true); // Retract roadbook
+OneButton bottomButton(2, true, true); // Trip down
+OneButton middleButton(3, true, true); // Trip up
+OneButton upperButton(4, true, true);
 
-#define LED 8
-#define NUM_BUTTONS 5
-
-const uint8_t BUTTON_PINS[NUM_BUTTONS] = {volUpButton, volDownButton, trackPrevButton, trackNextButton, extraButton};
-uint8_t lastStates[NUM_BUTTONS] = {HIGH, HIGH, HIGH, HIGH, HIGH};
 bool isOperational = true;
 
-void blinkLED();
-bool changeMode();
-void loopButtons(void);
-uint8_t checkButtonPress(int i, uint8_t state);
+
+void changeMode();
+void roadbookForward();
+void roadbookBackward();
+void tripBackward();
+void tripForward();
+void extra();
 
 void setup() {
     Serial.begin(115200);
     bleKeyboard.begin();
 
-    pinMode(LED, OUTPUT);
+    switchDownButton.setLongPressIntervalMs(300);
+    switchUpButton.setLongPressIntervalMs(300);
     
-    pinMode(volUpButton, INPUT_PULLUP);
-    pinMode(volDownButton, INPUT_PULLUP);
-    pinMode(trackPrevButton, INPUT_PULLUP);
-    pinMode(trackNextButton, INPUT_PULLUP);
-    pinMode(extraButton, INPUT_PULLUP);
+    switchDownButton.attachDuringLongPress(roadbookForward);
+    switchUpButton.attachDuringLongPress(roadbookBackward);
+    bottomButton.attachClick(tripBackward);
+    middleButton.attachClick(tripForward);
+    upperButton.attachClick(extra);
+    
+    upperButton.attachDoubleClick(changeMode);
 }
 
 void loop() {
 
-    if (changeMode()) {
-        isOperational = !isOperational;
-        if (isOperational) {
-            endOTA();
-            bleKeyboard.begin();
-        } else {
-            bleKeyboard.end();
-            beginOTA();
-        }
-    }
-
-    if (isOperational) {
-        loopButtons();
-    } else {
+    if (!isOperational)
         loopOTA();
-    }
 
-    blinkLED();
+    switchDownButton.tick();
+    switchUpButton.tick();
+    bottomButton.tick();
+    middleButton.tick();
+    upperButton.tick();
 }
 
-void blinkLED() {
-    int delaySeconds;
-    if (isOperational)
-        delaySeconds = 3000;
-    else
-        delaySeconds = 200;
-
-    digitalWrite(LED, HIGH);
-    delay(delaySeconds);
-    digitalWrite(LED, LOW);
-    delay(delaySeconds);
-}
-
-void loopButtons(void) {
-    if (!bleKeyboard.isConnected()) {
-        Serial.println("Not connected");
-        return;
-    }
-
-    for (int i = 0; i < NUM_BUTTONS; i++) {
-        uint8_t state = digitalRead(BUTTON_PINS[i]);
-        
-        if (state == LOW && lastStates[i] == HIGH) {
-            state = checkButtonPress(i, state);
-            Serial.print("Pressed ");
-            Serial.println(BUTTON_PINS[i]);
-        }
-
-        lastStates[i] = state;
+void changeMode() {
+    Serial.println("Change mode");
+    isOperational = !isOperational;
+    if (isOperational) {
+        endOTA();
+        bleKeyboard.begin();
+    } else {
+        bleKeyboard.end();
+        beginOTA();
     }
 }
 
-uint8_t checkButtonPress(int i, uint8_t state) {
-    switch (BUTTON_PINS[i]) {
-        case volUpButton:
-            bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
-            Serial.println("Press VOL UP");
-            break;
-        case volDownButton:
-            bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
-            Serial.println("Press VOL DOWN");
-            break;
-        case extraButton:
-            // bleKeyboard.write(KEY_END);
-            // isOperational = !isOperational;
-            // Serial.println("Press extra key");
-            break;
-        case trackPrevButton:
-            Serial.println("Start PREV TRACK");
-            while (state == LOW) {
-                bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
-                delay(100);
-                state = digitalRead(BUTTON_PINS[i]);
-            }
-            Serial.println("End PREV TRACK");
-            break;
-        case trackNextButton:
-            Serial.println("Start NEXT TRACK");
-            while (state == LOW) {
-                bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
-                delay(100);
-                state = digitalRead(BUTTON_PINS[i]);
-            }
-            Serial.println("End NEXT TRACK");
-            break;
-    }
-
-    return state;
+void roadbookForward() {
+    bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
+    Serial.println("Press roadbook forward");
 }
 
-bool changeMode(void) {
-    uint8_t state = digitalRead(BUTTON_PINS[4]);
-        
-    if (state == LOW && lastStates[4] == HIGH) {
-        Serial.print("Pressed ");
-        Serial.println(BUTTON_PINS[4]);
-        
-        lastStates[4] = state;
-        return true;
-    }
+void roadbookBackward() {
+    bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+    Serial.println("Press roadbook back");
+}
 
-    lastStates[4] = state;
-    return false;
+void tripForward() {
+    bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
+    Serial.println("Press trip forward");
+}
+
+void tripBackward() {
+    bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+    Serial.println("Press trip back");
+}
+
+void extra() {
+    Serial.println("Press extra");
 }
